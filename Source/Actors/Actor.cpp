f62086067534d71028a367ac5e9e84c74e4527ed
Nathan Lopes <nathan.s.lopes@ufv.br>
2024-04-05 00:38:55 -0300

    Actor ok Scene.h ok

---
+++ Source/Actors/Actor.cpp
@@ -0,0 +1,87 @@
+//
+// Created by nslop on 04/04/2024.
+//
+
+#include "Actor.h"
+#include "../Game.h"
+#include "../Scenes/Scene.h";
+
+#include <algorithm>
+
+Actor::Actor(Scene* scene) :
+    mState(ActorState::Active),
+    mPosition(Vector2::Zero),
+    mScale(1.0f),
+    mRotation(0.0f),
+    mScene(scene)
+{
+    mScene->GetGame()->AddActor(this);
+}
+
+Actor::~Actor()
+{
+    mScene->GetGame()->RemoveActor(this);
+
+    for(auto component : mComponents)
+    {
+        delete component;
+    }
+    mComponents.clear();
+}
+
+void Actor::Update(float deltaTime)
+{
+    if (mState == ActorState::Active)
+    {
+        for (auto comp : mComponents)
+        {
+            if(comp->IsEnabled())
+            {
+                comp->Update(deltaTime);
+            }
+        }
+
+        OnUpdate(deltaTime);
+    }
+}
+
+void Actor::OnUpdate(float deltaTime)
+{
+
+}
+
+void Actor::OnCollision()
+{
+
+}
+
+void Actor::Kill()
+{
+
+}
+
+void Actor::ProcessInput(const Uint8 *keyState)
+{
+    if(mState == ActorState::Active)
+    {
+        for(auto comp : mComponents)
+        {
+            comp->ProcessInput(keyState);
+        }
+
+        OnProcessInput(keyState);
+    }
+}
+
+void Actor::OnProcessInput(const Uint8 *keyState)
+{
+
+}
+
+void Actor::AddComponent(Component *c)
+{
+    mComponents.emplace_back(c);
+    std::sort(mComponents.begin(), mComponents.end(), [](Component* a, Component* b) {
+        return a->GetUpdateOrder() < b->GetUpdateOrder();
+    });
+}
--- Source/Game.cpp
+++ Source/Game.cpp
@@ -94,3 +94,223 @@ void Game::InitializeActors()
         }
     }
 }
+
+void Game::RunLoop() {
+
+    while (mIsRunning)
+    {
+        ProcessInput();
+        UpdateGame();
+        GenerateOutput();
+    }
+}
+
+void Game::ProcessInput()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+            case SDL_QUIT:
+                Quit();
+                break;
+        }
+    }
+
+    const Uint8* state = SDL_GetKeyboardState(nullptr);
+
+    for (auto actor : mActors)
+    {
+        actor->ProcessInput(state);
+    }
+
+    if (mScene)
+    {
+        mScene->ProcessInput(state);
+    }
+}
+
+void Game::UpdateGame()
+{
+    while(!SDL_TICKS_PASSED(SDL_GetTicks(), mTicksCount + 16));
+
+    float deltaTime = (SDL_GetTicks() - mTicksCount) / 1000.0f;
+    if(deltaTime > 0.05f)
+    {
+        deltaTime = 0.05f;
+    }
+
+    mTicksCount = SDL_GetTicks();
+
+    if(!mChangeScene)
+    {
+        UpdateActors(deltaTime);
+
+        UpdateCamera();
+    }
+    else
+    {
+        UnloadActors();
+        InitializeActors();
+        mChangeScene = false;
+    }
+}
+
+void Game::UpdateCamera()
+{
+
+}
+
+void Game::UpdateActors(float deltaTime) {
+
+    mUpdatingActors = true;
+    for (auto actor : mActors)
+    {
+        actor->Update(deltaTime);
+    }
+    mUpdatingActors = false;
+
+    for (auto pending : mPendingActors)
+    {
+        mActors.emplace_back(pending);
+    }
+    mPendingActors.clear();
+
+    std::vector<Actor*> deadActors;
+    for (auto actor : mActors)
+    {
+        if (actor->GetState() == ActorState::Destroy)
+        {
+            deadActors.emplace_back(actor);
+        }
+    }
+
+    for (auto actor : deadActors)
+    {
+        delete actor;
+    }
+}
+
+void Game::AddActor(Actor* actor)
+{
+    if (mUpdatingActors)
+    {
+        mPendingActors.emplace_back(actor);
+    }
+    else
+    {
+        mActors.emplace_back(actor);
+    }
+}
+
+void Game::RemoveActor(Actor* actor)
+{
+    auto iter = std::find(mPendingActors.begin(), mPendingActors.end(), actor);
+    if (iter != mPendingActors.end())
+    {
+        // Swap to end of vector and pop off (avoid erase copies)
+        std::iter_swap(iter, mPendingActors.end() - 1);
+        mPendingActors.pop_back();
+    }
+
+    iter = std::find(mActors.begin(), mActors.end(), actor);
+    if (iter != mActors.end())
+    {
+        // Swap to end of vector and pop off (avoid erase copies)
+        std::iter_swap(iter, mActors.end() - 1);
+        mActors.pop_back();
+    }
+}
+
+void Game::AddDrawable(class DrawComponent *drawable)
+{
+    mDrawables.emplace_back(drawable);
+
+    std::sort(mDrawables.begin(), mDrawables.end(),[](DrawComponent* a, DrawComponent* b) {
+        return a->GetDrawOrder() < b->GetDrawOrder();
+    });
+}
+
+void Game::RemoveDrawable(class DrawComponent *drawable)
+{
+    auto iter = std::find(mDrawables.begin(), mDrawables.end(), drawable);
+    mDrawables.erase(iter);
+}
+
+void Game::AddCollider(class CircleColliderComponent* collider)
+{
+    mColliders.emplace_back(collider);
+}
+
+void Game::RemoveCollider(CircleColliderComponent* collider)
+{
+    auto iter = std::find(mColliders.begin(), mColliders.end(), collider);
+    mColliders.erase(iter);
+}
+
+void Game::GenerateOutput()
+{
+    // Set draw color to black
+    SDL_SetRenderDrawColor(mRenderer, 0, 0, 0, 255);
+
+    // Clear back buffer
+    SDL_RenderClear(mRenderer);
+
+    for (auto drawable : mDrawables)
+    {
+        if (drawable->IsVisible())
+        {
+            drawable->Draw(mRenderer);
+        }
+    }
+
+    // Swap front buffer and back buffer
+    SDL_RenderPresent(mRenderer);
+}
+
+SDL_Texture* Game::LoadTexture(const std::string& texturePath) {
+
+    auto loadedIMG = IMG_Load(texturePath.c_str());
+    if (loadedIMG == nullptr) {
+        return nullptr;
+    }
+
+
+    auto textureFromSur = SDL_CreateTextureFromSurface(mRenderer, loadedIMG);
+    SDL_FreeSurface(loadedIMG);
+    if (!textureFromSur)
+    {
+        return nullptr;
+    }
+
+
+    return textureFromSur;
+}
+
+void Game::Shutdown()
+{
+    UnloadActors();
+
+    SDL_DestroyRenderer(mRenderer);
+    SDL_DestroyWindow(mWindow);
+    SDL_Quit();
+}
+
+void Game::UnloadActors()
+{
+    while (!mActors.empty())
+    {
+        delete mActors.back();
+    }
+
+    delete mScene;
+    mScene = nullptr;
+}
+
+void Game::SetScene(Game::GameScene scene)
+{
+    mAudio->StopAllSounds();
+    mGameState = scene;
+    mChangeScene = true;
+}
