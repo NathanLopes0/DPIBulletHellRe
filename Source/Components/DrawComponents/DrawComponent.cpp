b4d0aea74b713bc2bd90f98a6fa95069f1a81b4a
Nathan Lopes <nathan.s.lopes@ufv.br>
2024-04-10 13:35:03 -0300

    Start Refactor

--- Source/Components/DrawComponents/DrawComponent.cpp
+++ Source/Components/DrawComponents/DrawComponent.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "DrawComponent.h"
+#include "../../Actors/Actor.h"
+#include "../../Scenes/Scene.h"
+#include "../../Game.h"
+
+DrawComponent::DrawComponent(class Actor *owner, int drawOrder)
+        : Component(owner),
+        mDrawOrder(drawOrder),
+        mIsVisible(true)
+{
+    mOwner->GetScene()->GetGame()->AddDrawable(this);
+}
+
+DrawComponent::~DrawComponent()
+{
+    mOwner->GetScene()->GetGame()->RemoveDrawable(this);
+}
+
+void DrawComponent::Draw(class SDL_Renderer *renderer)
+{
+
+}
--- Source/Components/DrawComponents/DrawComponent.h
+++ Source/Components/DrawComponents/DrawComponent.h
@@ -4,10 +4,23 @@
 
 #ifndef DPIBULLETHELLRE_DRAWCOMPONENT_H
 #define DPIBULLETHELLRE_DRAWCOMPONENT_H
+#include "../Component.h"
 
+class DrawComponent : public Component {
+public:
+    explicit DrawComponent(class Actor* owner, int drawOrder = 100);
+    ~DrawComponent() override;
 
-class DrawComponent {
+    virtual void Draw(class SDL_Renderer* renderer);
 
+    bool IsVisible() const { return mIsVisible; }
+    void SetIsVisible(const bool isVisible) { mIsVisible = isVisible; }
+
+    int GetDrawOrder() const { return mDrawOrder; }
+
+protected:
+    bool mIsVisible;
+    int mDrawOrder;
 };
 
 
