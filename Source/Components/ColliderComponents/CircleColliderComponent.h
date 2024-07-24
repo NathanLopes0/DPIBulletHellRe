41da6a241d7a4e1c2c53bd79054c650af5576487
Nathan Lopes <nathan.s.lopes@ufv.br>
2024-07-23 23:40:13 -0300

    23/07 23:39
    complete colliders and main drawComponents

---
+++ Source/Components/ColliderComponents/CircleColliderComponent.h
@@ -0,0 +1,29 @@
+//
+// Created by nslop on 23/07/2024.
+//
+
+#ifndef DPIBULLETHELLRE_CIRCLECOLLIDERCOMPONENT_H
+#define DPIBULLETHELLRE_CIRCLECOLLIDERCOMPONENT_H
+
+#include "../Component.h"
+#include "../../Math.h"
+
+
+class CircleColliderComponent : public Component {
+
+public:
+    CircleColliderComponent(class Actor* owner, float radius, int updateOrder = 10);
+
+    void SetRadius(float radius) { mRadius = radius; }
+    float GetRadius() const;
+
+    const Vector2& GetCenter() const;
+
+    bool Intersect(const CircleColliderComponent& b) const;
+
+private:
+    float mRadius;
+};
+
+
+#endif //DPIBULLETHELLRE_CIRCLECOLLIDERCOMPONENT_H
