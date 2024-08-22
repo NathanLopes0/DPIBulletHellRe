0ad4ea84409340ab12c1ac432e006d0205f80d71
Nathan Lopes <nathan.s.lopes@ufv.br>
2024-08-22 00:51:22 -0300

    reset Scene variable changes, add StageSelect, no impl

---
+++ Source/Scenes/StageSelect.h
@@ -0,0 +1,21 @@
+//
+// Created by nslop on 21/08/2024.
+//
+
+#ifndef DPIBULLETHELLRE_STAGESELECT_H
+#define DPIBULLETHELLRE_STAGESELECT_H
+
+#include "../Game.h"
+
+class StageSelect {
+
+public:
+    StageSelect(Game* game);
+
+private:
+
+
+};
+
+
+#endif //DPIBULLETHELLRE_STAGESELECT_H
